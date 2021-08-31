16cafb8274cd650d0d0f325349e71c22cd2dd438
JhonatanRSantos <jhonatanatm@gmail.com>
2021-08-31 13:14:48 -0300

    Add the HaspMap folder and add the first problem

---
+++ hashmaps/01/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/JhonatanRSantos/go-exercices/common"
+)
+
+func main() {
+	magazine := common.ReadNextLine()
+	note := common.ReadNextLine()
+
+	fmt.Println(
+		solution(magazine, note),
+	)
+}
+
+func solution(magazine string, note string) string {
+	check := true
+	magazineWords := map[string]int{}
+
+	for _, v := range strings.Split(magazine, " ") {
+		if _, ok := magazineWords[v]; ok {
+			magazineWords[v] += 1
+		} else {
+			magazineWords[v] = 1
+		}
+	}
+
+	for _, v := range strings.Split(note, " ") {
+		if value, ok := magazineWords[v]; ok && value > 0 {
+			magazineWords[v] -= 1
+		} else {
+			check = false
+			break
+		}
+	}
+
+	if check {
+		return "Yes"
+	} else {
+		return "No"
+	}
+}
